e5fdff28eeac21fd68513189d1428c697d5149ee
Matilde Russo <matilderusso@MacBook-Air-di-Matilde.local>
2021-04-02 17:09:11 +0200

    modifica attributi utente

--- include/classe_utente.php
+++ include/classe_utente.php
@@ -1,56 +1,51 @@
 <?php
    class Utente {
-       private $idUt;
-       private $nomeUt;
-       private $cognomeUT;
+       private $nome;
+       private $cognome;
        private $email;
-       private $dataIscr;
-       private $tipoAbb;  //array
+       private $data_iscrizione;
+       private $tipo_abbonamento; //array
 
-       public function __construct($id, $nome, $cognome, $em, $data, $abb) {
-           $this->idUt = $id;
-           $this->nomeUt = $nome;
-           $this->cognomeUt = $cognome;
-           $this->email = $em;
-           $this->dataIscr = $data;
-           $this->tipoAbb = $abb;
+       public function __construct($nome, $cognome, $email, $data_iscrizione, $tipo_abbonamento) {
+           $this->nome = $nome; //nome di questa classe (this)
+           $this->cognome = $cognome;
+           $this->email = $email;
+           $this->data_iscrizione = $data_iscrizione;
+           $this->tipo_abbonamento = $tipo_abbonamento;
        }
+//fare prima le get che le set x tutti 
 
-       public function setIdUt($id) {
-           $this->idUt = $id;
+
+
+       public function setNome($nome) {   //x modificare attributo dell'oggetto
+           $this->nome = $nome;
        }
-       public function getIdUt() {
-           return $this->IdUt;
+       public function getNome() {  //x estrarre dati dall'oggetto
+           return $nome;    //posso anche togliere il this e lasciare $nome: non ci sono dubbi su cosa sia nome, l'unico nome che conosce è l'attributo dell'ogg utente perchè è dichiarato nella mia classe
        }
-       public function setNomeUt($nome) {
-           $this->nomeUt = $nome;
+       public function setCognome($cognome) {
+           $this->cognome = $cognome;
        }
-       public function getNomeUt() {
-           return $this->nomeUt;
+       public function getCognome() {
+           return $cognome;
        } 
-       public function setCognomeUt($cognome) {
-           $this->cognomeUt = $cognome;
-       }
-       public function getCognomeUt() {
-           return $this->cognomeUt = $cognome;
-       }
-       public function setEmail($em) {
-           $this->email = $em;
+       public function setEmail($email) {
+           $this->email = $email;
        }
        public function getEmail() {
-           return $this->email = $em;
+           return $email;
        }
-       public function setDataIscr($data) {
-           $this->dataIscr = $data;
+       public function setData_iscrizione($data_iscrizione) {
+           $this->data_iscrizione = $data_iscrizione;
        }
-       public function getDataIscr() {
-           return $this->dataIscr = $data;
+       public function getData_iscrizione() {
+           return $data_iscrizione;
        }
-       public function setTipoAbb($abb) {
-           $this->tipoAbb = $abb;
+       public function setTipo_abbonamento($tipo_abbonamento) {
+           $this->tipo_abbonamento = $tipo_abbonamento;
        }
-       public function getTipoAbb() {
-           return $this->tipoAbb = $abb;
+       public function getTipo_abbonamento() {
+           return $tipo_abbonamento;
        }
    }
 
